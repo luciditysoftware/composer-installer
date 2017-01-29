832d5c3034ef1846ccb98969bb39d8e4454e3452
Tristan Kenney <tristan@kenney.co>
2017-01-29 22:00:20 +1100

    Off by one

--- src/Composer/ModuleInstaller.php
+++ src/Composer/ModuleInstaller.php
@@ -14,7 +14,7 @@ class ModuleInstaller extends LibraryInstaller
 
     public function getInstallPath(PackageInterface $package)
     {
-        $prefix = substr($package->getPrettyName(), 0, 17);
+        $prefix = substr($package->getPrettyName(), 0, 16);
         if ('lucidity/module-' !== $prefix) {
             throw new InvalidArgumentException("Not a valid Lucidity module; package must be prefixed with 'lucidity/module-'");
         }
